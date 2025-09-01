b56a0a7f1b65d613a3158287a7ed40df2d91a7c3
Parth Hande <hparth67@gmail.com>
2025-09-02 02:54:15 +0530

    completed the full stack project

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,30 @@
-import express from "express"
-import helmet from "helmet"
-import morgan from "morgan"
-import cors from "cors"
-import dotenv from "dotenv"
-import productRoutes from "./routes/productRoutes.js"
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
-import { aj } from "./lib/arcjet.js"
+import { aj } from "./lib/arcjet.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(express.json())
-app.use(cors())
-app.use(helmet()) // helmet is a security middleware that helps you protect your app by setting various HTTP headers
-app.use(morgan("dev"))  //log the request
+app.use(express.json());
+app.use(cors());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
+app.use(morgan("dev")); // log the requests
 
 // apply arcjet rate-limit to all routes
-
 app.use(async (req, res, next) => {
   try {
     const decision = await aj.protect(req, {
@@ -49,27 +55,39 @@ app.use(async (req, res, next) => {
   }
 });
 
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // server our react app
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 async function initDB() {
-    try {
-        await sql`
-        CREATE TABLE IF NOT EXISTS products (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            image VARCHAR(255) NOT NULL,
-            price DECIMAL(10, 2) NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )`;
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
 
-        console.log("Database initialized successfully")
-    } catch (error) {
-        console.log("Error initDB", error)
-    }
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
 }
 
 initDB().then(() => {
-    app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}...`)
-})
-})
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  });
+});
+
+
